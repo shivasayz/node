ee99458e5b6a5963db18217f987900109d636ab5
shivasayz <writeforshivaa@gmail.com>
2025-12-22 20:16:04 +0530

    data added to db from code

--- server.js
+++ server.js
@@ -6,23 +6,55 @@ import express from 'express';
 import morgan from 'morgan';
 import app from './app.js';
 
-
 // console.log(process.env.NODE_ENV); // now it will print "development"
-const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-console.log(db);
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-mongoose.connect(db, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-}).then(con => {
-  console.log(con.connection);
-  console.log("DB connected successful")
-});
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connected successful'));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'neem-leaf',
+  rating: 4.8,
+  price: 870,
+});
+
+testTour
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => console.log('Error ⚠️', err));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
