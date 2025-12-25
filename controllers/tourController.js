037446a385342f10c3666a8210f4e0f1037e83bc
shivasayz <writeforshivaa@gmail.com>
2025-12-25 15:50:35 +0530

    APIfeatures moved to utlis

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,5 @@
 import { Tour } from '../models/modelsExport.js';
-import qs from 'qs';
+import APIFeatures from "./../utils/apiFeatures.js";
 
 // middlewares
 const aliasTopTours = (req, res, next) => {
@@ -15,61 +15,6 @@ const aliasTopTours = (req, res, next) => {
 
   next();
 };
-
-class APIFeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
-
-  filter() {
-    // 1A. filtering
-    const queryObj = { ...qs.parse(this.queryString) };
-    const excludeFields = ['sort', 'page', 'limit', 'fields'];
-    excludeFields.forEach((el) => delete queryObj[el]);
-
-    // 1B. advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
-    this.query = this.query.find(JSON.parse(queryStr));
-
-    return this;
-  }
-
-  sort() {
-    // 2. sorting
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query = this.query.sort(sortBy);
-    } else {
-      this.query = this.query.sort('-createdAt');
-    }
-
-    return this;
-  }
-
-  limitFields() {
-    // 3. limiting fields
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
-      this.query = this.query.select(fields);
-    } else {
-      this.query.select('-__v');
-    }
-
-    return this;
-  }
-
-  paginate() {
-    // 4. pagination
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    this.query = this.query.skip(skip).limit(limit);
-    return this;
-  }
-}
 
 const getAllTours = async (req, res) => {
   try {
